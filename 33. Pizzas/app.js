78c004bf124c4bc27eeab747fb804abdcacf35e0
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:32:20 -0700

    Add files via upload

---
+++ 33. Pizzas/app.js
@@ -0,0 +1,5 @@
+var pizzas = ["cheese", "barbecue", "italian"];
+pizzas.forEach(function (pizza) {
+    console.log("I like ".concat(pizza, " pizza."));
+});
+console.log("I really love pizza!");
---
+++ 33. Pizzas/app.ts
@@ -0,0 +1,6 @@
+let pizzas: string[] = ["cheese", "barbecue", "italian"];
+
+pizzas.forEach(pizza => {
+    console.log(`I like ${pizza} pizza.`);
+});
+console.log("I really love pizza!");
